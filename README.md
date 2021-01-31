09e6c6fb1e7708753d4eb2a68790ae37527822cd
Samantha Lovisolo <samantha.lovisolo@wisemotions.com>
2021-01-31 03:10:05 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# gitgo
+
+[![GitHub Super-Linter](https://github.com/sam959/gitgo/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
+
+Git on the go
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/samanthalovisolo/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/samanthalovisolo/AndroidStudioProjects/git_go"
+export "FLUTTER_TARGET=/Users/samanthalovisolo/AndroidStudioProjects/git_go/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "EXCLUDED_ARCHS=arm64"
+export "DART_DEFINES=FLUTTER_WEB_AUTO_DETECT%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/samanthalovisolo/AndroidStudioProjects/git_go/.dart_tool/package_config.json"
