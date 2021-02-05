db702943112bf60b3c3cbdb4b6ba6a558d4006a9
Sam <56480741+sam959@users.noreply.github.com>
2021-02-05 23:43:33 +0100

    Merge develop into master (#10)
    * Add workflow file
    
    * Update workflow file
    
    * Update workflow
    
    * Update project
    
    * Rename test
    
    * Add test
    
    * Update workflow files
    
    * Add flutter ci
    
    * Fix indent
    
    * Update ci and dependency
    
    * Update  dart sdk
    
    * Upgrade flutter in ci!
    
    * Fix tests and null safety
    
    * Run test w/o null safety
    
    * fix test
    
    * Update workflow

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'GitGo Demo Home Page'),
+      home: MyHomePage(key: Key(""),title: 'GitGo Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({required Key key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -39,24 +39,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
   _MyHomePageState(String title) {
     this.title = title;
   }
 
-  String title;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String title = "";
 
   @override
   Widget build(BuildContext context) {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,18 +5,16 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:gitgo/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('Login to Github'), findsOneWidget);
+    expect(find.text('Log into Github'), findsOneWidget);
     expect(find.text('1'), findsNothing);
   });
 }
