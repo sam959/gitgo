be9f921199baff1adf78d53a4a6c7f02870fda80
sam959 <liekemonster@gmail.com>
2021-02-19 01:04:12 +0100

    Add repository package and impl in app

---
+++ lib/src/app.dart
@@ -0,0 +1,93 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gitgo/src/inbound/configuration/config_environment_widget.dart';
+import 'package:gitgo/src/outbound/api/viewer.data.gql.dart';
+import 'package:gitgo/src/outbound/api/viewer.req.gql.dart';
+import 'package:gitgo/src/outbound/auth/auth.dart';
+import 'package:gql_exec/gql_exec.dart';
+import 'package:gql_link/gql_link.dart';
+import 'package:window_to_front/window_to_front.dart';
+
+import '../src/domain/github_summary.dart';
+import 'inbound/configuration/environment_config.dart';
+
+class MyApp extends StatelessWidget {
+  final EnvironmentConfig config;
+  final AuthenticationRepository authenticationRepository;
+
+  const MyApp({@required this.config, @required this.authenticationRepository});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GitGo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: EnvironmentConfigWidget(
+          config: config, child: MyHomePage(title: 'Welcome to GitGo')),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(title: "GitGo Dashboard");
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  _MyHomePageState({this.title});
+
+  EnvironmentConfigWidget environmentConfigWidget;
+
+  String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return GithubLoginWidget(
+        builder: (context, client) {
+          WindowToFront.activate();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(title),
+            ),
+            body: GitHubSummary(client: client),
+          );
+        },
+        githubClientId: EnvironmentConfigWidget.of(context)
+            .config
+            .environment['githubClientId'],
+        githubClientSecret: EnvironmentConfigWidget.of(context)
+            .config
+            .environment['githubClientSecret'],
+        githubScopes: EnvironmentConfigWidget.of(context)
+            .config
+            .environment['githubScopes']
+            .split(","));
+  }
+}
+
+Future<$ViewerDetail$viewer> viewerDetail(Link link) async {
+  var result = await link.request(ViewerDetail((b) => b)).first;
+  if (result.errors != null && result.errors.isNotEmpty) {
+    throw QueryException(result.errors);
+  }
+  return $ViewerDetail(result.data).viewer;
+}
+
+class QueryException implements Exception {
+  QueryException(this.errors);
+
+  List<GraphQLError> errors;
+
+  @override
+  String toString() {
+    return 'Query Exception: ${errors.map((err) => '$err').join(',')}';
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,16 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:gitgo/src/app.dart';
 import 'package:gitgo/src/inbound/configuration/environment_config.dart';
-import 'package:gitgo/src/main.dart';
 
 void main() {
   testWidgets('Smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
 
     var env = await EnvironmentConfig.forEnvironment('dev');
-    await tester.pumpWidget(MyApp(env));
+    await tester.pumpWidget(MyApp(
+        config: env, authenticationRepository: AuthenticationRepository()));
 
     // Verify that our counter starts at 0.
     expect(find.text('Log into Github'), findsOneWidget);
